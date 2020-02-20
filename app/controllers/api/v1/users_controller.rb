385f2903a2d52eddf22f1df64093c3b861f05688
bdc5435 <bdc5435@rit.edu>
2020-02-20 14:55:33 -0500

    added create update and show to users_controller.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,26 @@ class Api::V1::UsersController < ApplicationController
     render json: User.all
   end
 
-  def user_params
-    params.require(:user).permit(:email,:password,:firstName,:lastName)
+  def create
+    @json = JSON.parse(request.body.read)
+    user = User.new
+    user.email = @json['email']
+    user.password = @json['password']
+    user.firstName = @json['firstName']
+    user.lastName = @json['lastName']
+    user.save
+    render user.to_json(:except=>password)
+  end
+  def update
+    @id = params[:id]
+    @body = JSON.parse(request.body.read)
+    user = User.find_by(_id: @id)
+    user.puzzles << @body
+    render json: user
+  end
+  def show
+    @id = params[:id]
+    user = User.find_by(_id: @id)
+    render json: user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :login
       resources :users
-  end
+      get 'users', to: 'api/v1/users#index'
+      get 'users/:id', to: 'api/v1/users#show'
+      post 'users', to: 'api/v1/users#create'
+      put 'users/:id', to: 'api/v1/users#update'
+    end
   end
 end
